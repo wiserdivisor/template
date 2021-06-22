baa50a15676857bab850ec2343d649a0649c2122
wiserdivisor <dat0user@gmail.com>
2021-06-22 20:49:02 +0530

    commit_10 - width fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,59 +18,58 @@ import { Footer } from "./footer";
 const Layout = ({ pageTitle, children }) => {
   return (
     <main className={container}>
-      <div>
-        <Helmet
-          htmlAttributes={{ lang: "en" }}
-          meta={[
-            {
-              name: "viewport",
-              content: "width=device-width, initial-scale=1.0",
-            },
-            {
-              name: "description",
-              content: "EV Course - Electric Vehicles Assembly, Maintenence & Repairs Training. Includes sizing, testing, troubleshooting and component-level repairs of battery, EV Controller, DC to DC converter and battery pack assembly. EV motors and hubs.",
-            },
-            { property: "og:type", content: "website" },
-            {
-              property: "og:title",
-              content: "Academy of Electric Vehicles Research and Technology.",
-            },
-            {
-              property: "og:description",
-              content:
-                "EV Course - Electric Vehicles Assembly, Maintenence & Repairs Training. Includes sizing, testing, troubleshooting and component-level repairs of battery, EV Controller, DC to DC converter and battery pack assembly. EV motors and hubs.",
-            },
-            { property: "og:url", content: "https://trainingev.gatsbyjs.io" },
-            { property: "og:image", content: `${logo}` },
-          ]}
-          link={[{ rel: "icon", type: "image/x-icon", href: `${favicon}` }]}
+      <Helmet
+        htmlAttributes={{ lang: "en" }}
+        meta={[
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+          },
+          {
+            name: "description",
+            content:
+              "EV Course - Electric Vehicles Assembly, Maintenence & Repairs Training. Includes sizing, testing, troubleshooting and component-level repairs of battery, EV Controller, DC to DC converter and battery pack assembly. EV motors and hubs.",
+          },
+          { property: "og:type", content: "website" },
+          {
+            property: "og:title",
+            content: "Academy of Electric Vehicles Research and Technology.",
+          },
+          {
+            property: "og:description",
+            content:
+              "EV Course - Electric Vehicles Assembly, Maintenence & Repairs Training. Includes sizing, testing, troubleshooting and component-level repairs of battery, EV Controller, DC to DC converter and battery pack assembly. EV motors and hubs.",
+          },
+          { property: "og:url", content: "https://trainingev.gatsbyjs.io" },
+          { property: "og:image", content: `${logo}` },
+        ]}
+        link={[{ rel: "icon", type: "image/x-icon", href: `${favicon}` }]}
+      >
+        <title>{pageTitle} - ÆVRT</title>
+        <meta property="og:url" content="https://trainingev.gatsbyjs.io" />
+      </Helmet>
+      <div className={headerDiv}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          <title>{pageTitle} - ÆVRT</title>
-          <meta property="og:url" content="https://trainingev.gatsbyjs.io" />
-        </Helmet>
-        <div className={headerDiv}>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div className={siteLogo}>
-              <StaticImage src="../images/logo.png" alt="" />
-            </div>
-            <p className={headerText}>{pageTitle}</p>
+          <div className={siteLogo}>
+            <StaticImage src="../images/logo.png" alt="" />
           </div>
-          {pageTitle[0] === "A" ? (
-            <>
-              <hr className={divider} />
-              <p className={headerSubtext}>
-                A KBP Trust & Institute of Engineering and Applied Sciences
-                Initiative
-              </p>
-            </>
-          ) : null}
+          <p className={headerText}>{pageTitle}</p>
         </div>
+        {pageTitle[0] === "A" ? (
+          <>
+            <hr className={divider} />
+            <p className={headerSubtext}>
+              A KBP Trust & Institute of Engineering and Applied Sciences
+              Initiative
+            </p>
+          </>
+        ) : null}
       </div>
       <Navbar title={pageTitle} />
       <div style={{ minWidth: 500, maxWidth: "50%", margin: "auto" }}>
